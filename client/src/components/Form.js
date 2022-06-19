6923bb314523986ce460c4824cd647bf9248c498
Anton Zaitsev <zaitsev.anton@outlook.com>
2022-06-19 13:56:54 +0200

    [FEATURE] Add backend hcaptcha verification

--- client/src/components/Form.js
+++ client/src/components/Form.js
@@ -1,126 +1,135 @@
-import styled from "styled-components"
-import { useState } from "react"
-import HCaptcha from "@hcaptcha/react-hcaptcha"
-import { useNavigate } from "react-router-dom"
+import styled from "styled-components";
+import { useState } from "react";
+import HCaptcha from "@hcaptcha/react-hcaptcha";
+import { useNavigate } from "react-router-dom";
 
 const StyledForm = styled.form`
-	grid-column: span 6;
-	display: flex;
-	flex-direction: column;
-	label {
-		display: flex;
-		flex-direction: column;
-	}
-`
+  grid-column: span 6;
+  display: flex;
+  flex-direction: column;
+  label {
+    display: flex;
+    flex-direction: column;
+  }
+`;
 
 const Form = (props) => {
-	let navigate = useNavigate()
+  let navigate = useNavigate();
 
-	const [firstName, setFirstName] = useState("")
-	const [lastName, setLastName] = useState("")
-	const [mail, setMail] = useState("")
-	const [amount, setAmount] = useState(1)
-	const [termsCondition, setTermsCondition] = useState(false)
+  const [firstName, setFirstName] = useState("");
+  const [lastName, setLastName] = useState("");
+  const [mail, setMail] = useState("");
+  const [amount, setAmount] = useState(1);
+  const [termsCondition, setTermsCondition] = useState(false);
 
-	const [token, setToken] = useState(0)
-	const [ekey, setEkey] = useState(0)
+  const [token, setToken] = useState(0);
+  const [ekey, setEkey] = useState(0);
 
-	const data = {
-		firstName,
-		lastName,
-		mail,
-		amount,
-		termsCondition,
-		token,
-		ekey,
-	}
+  const data = {
+    firstName,
+    lastName,
+    mail,
+    amount,
+    termsCondition,
+    token,
+    ekey,
+  };
 
-	const onSubmit = (event) => {
-		if (token) {
-			console.log(data)
-			event.preventDefault()
-			// navigate("/tickets/captcha")
-		}
-		if (!token && !ekey) {
-			alert("Füllen Sie bitte das Captcha aus.")
-		}
-	}
+  const onSubmit = (event) => {
+    if (token) {
+      console.log(data);
 
-	const sitekey = "90ac8810-80ba-42f6-abd9-c7ab8cef95e1"
-	// const sitekey = "10000000-ffff-ffff-ffff-000000000001"
+      fetch("/api/captcha", {
+        method: "POST",
+        headers: {
+          Accept: "application/json",
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(data),
+      });
 
-	function handleVerificationSuccess(token, ekey) {
-		setToken(token)
-		setEkey(ekey)
-	}
+      // navigate("/tickets/captcha")
+    }
+    if (!token && !ekey) {
+      alert("Füllen Sie bitte das Captcha aus.");
+    }
 
-	return (
-		<StyledForm onSubmit={onSubmit}>
-			<label>
-				Vorname
-				<input
-					type="text"
-					required
-					name="firstName"
-					value={firstName}
-					onChange={(e) => setFirstName(e.target.value)}
-				/>
-			</label>
-			<label>
-				Nachname
-				<input
-					type="text"
-					required
-					name="lastName"
-					value={lastName}
-					onChange={(e) => setLastName(e.target.value)}
-				/>
-			</label>
-			<label>
-				E-Mail
-				<input
-					type="email"
-					required
-					name="mail"
-					value={mail}
-					onChange={(e) => setMail(e.target.value)}
-				/>
-			</label>
-			<label>
-				Anzahl
-				<input
-					type="number"
-					min="1"
-					required
-					name="amount"
-					value={amount}
-					onChange={(e) => setAmount(e.target.value)}
-				/>
-			</label>
+    event.preventDefault();
+  };
 
-			<label>
-				AGB
-				<input
-					type="checkbox"
-					required
-					name="termsCondition"
-					value={termsCondition}
-					onChange={(e) => setTermsCondition(e.target.value)}
-				/>
-			</label>
-			<label>
-				<input type="submit" />
-			</label>
-			{/* {props.children} */}
-			<HCaptcha
-				sitekey={sitekey}
-				theme="dark"
-				onVerify={(token, ekey) =>
-					handleVerificationSuccess(token, ekey)
-				}
-			/>
-		</StyledForm>
-	)
-}
+  const sitekey = "90ac8810-80ba-42f6-abd9-c7ab8cef95e1";
+  // const sitekey = "10000000-ffff-ffff-ffff-000000000001";
 
-export default Form
+  function handleVerificationSuccess(token, ekey) {
+    setToken(token);
+    setEkey(ekey);
+  }
+
+  return (
+    <StyledForm onSubmit={onSubmit}>
+      <label>
+        Vorname
+        <input
+          type="text"
+          required
+          name="firstName"
+          value={firstName}
+          onChange={(e) => setFirstName(e.target.value)}
+        />
+      </label>
+      <label>
+        Nachname
+        <input
+          type="text"
+          required
+          name="lastName"
+          value={lastName}
+          onChange={(e) => setLastName(e.target.value)}
+        />
+      </label>
+      <label>
+        E-Mail
+        <input
+          type="email"
+          required
+          name="mail"
+          value={mail}
+          onChange={(e) => setMail(e.target.value)}
+        />
+      </label>
+      <label>
+        Anzahl
+        <input
+          type="number"
+          min="1"
+          required
+          name="amount"
+          value={amount}
+          onChange={(e) => setAmount(e.target.value)}
+        />
+      </label>
+
+      <label>
+        AGB
+        <input
+          type="checkbox"
+          required
+          name="termsCondition"
+          value={termsCondition}
+          onChange={(e) => setTermsCondition(e.target.value)}
+        />
+      </label>
+      <label>
+        <input type="submit" />
+      </label>
+      {/* {props.children} */}
+      <HCaptcha
+        sitekey={sitekey}
+        theme="dark"
+        onVerify={(token, ekey) => handleVerificationSuccess(token, ekey)}
+      />
+    </StyledForm>
+  );
+};
+
+export default Form;
--- server.js
+++ server.js
@@ -1,19 +1,41 @@
-const express = require("express")
+require("dotenv").config();
+const express = require("express");
+const useragent = require("express-useragent");
 
-const app = express()
-var useragent = require("express-useragent")
+const app = express();
 
-app.use(useragent.express())
+app.use(express.json());
+app.use(useragent.express());
 
 app.get("/api/useragent", (req, res) => {
-	res.send(req.useragent)
-})
+  res.send(req.useragent);
+});
 
+app.post("/api/captcha", (req, res) => {
+  console.log(req.body);
+
+  // verify captcha
+  fetch("https://hcaptcha.com/siteverify", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/x-www-form-urlencoded;charset=UTF-8",
+    },
+    body: `secret=${encodeURIComponent(
+      process.env.HCAPTCHA_SECRET
+    )}&response=${encodeURIComponent(req.body.token)}`,
+  })
+    .then((response) => response.json())
+    .then((data) => {
+      console.log(data);
+    });
+
+  res.send("ok");
+});
 
 if (process.env.NODE_ENV === "production") {
-	app.use(express.static("client/build"))
+  app.use(express.static("client/build"));
 }
 
-const PORT = process.env.PORT || 8000
+const PORT = process.env.PORT || 8000;
 
-app.listen(PORT, () => console.log("Server listen on port " + PORT))
+app.listen(PORT, () => console.log("Server listen on port " + PORT));
