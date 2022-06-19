b69f76ea97e1e87468338be2e18188ad2dd4fc61
calvinhnzr <52936430+calvinhnzr@users.noreply.github.com>
2022-06-19 15:40:33 +0200

    [BUGFIX] change sitekey

--- client/src/components/Form.js
+++ client/src/components/Form.js
@@ -90,43 +90,43 @@ const Form = (props) => {
 	}
 
 	const onSubmit = (event) => {
-    if (token) {
-      // console.log(data);
+		if (token) {
+			// console.log(data);
 
-      fetch("/api/captcha", {
-        method: "POST",
-        headers: {
-          Accept: "application/json",
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify(data),
-      }).then((response) => {
-        if (response.status === 200) {
-          console.log("Captcha verifiziert");
-        } else {
-          console.log("Captcha nicht verifiziert");
-        }
-      });
+			fetch("/api/captcha", {
+				method: "POST",
+				headers: {
+					Accept: "application/json",
+					"Content-Type": "application/json",
+				},
+				body: JSON.stringify(data),
+			}).then((response) => {
+				if (response.status === 200) {
+					console.log("Captcha verifiziert")
+				} else {
+					console.log("Captcha nicht verifiziert")
+				}
+			})
 
-      captchaRef.current.resetCaptcha();
-      setFirstName("");
-      setLastName("");
-      setMail("");
-      setAmount(1);
-      setTermsCondition(false);
-      setToken(0);
-      setEkey(0);
-    }
+			captchaRef.current.resetCaptcha()
+			setFirstName("")
+			setLastName("")
+			setMail("")
+			setAmount(1)
+			setTermsCondition(false)
+			setToken(0)
+			setEkey(0)
+		}
 
-    if (!token && !ekey) {
-      alert("Füllen Sie bitte das Captcha aus.");
-    }
+		if (!token && !ekey) {
+			alert("Füllen Sie bitte das Captcha aus.")
+		}
 
-    event.preventDefault();
-  };
+		event.preventDefault()
+	}
 
-	// const sitekey = "90ac8810-80ba-42f6-abd9-c7ab8cef95e1"
-	const sitekey = "10000000-ffff-ffff-ffff-000000000001"
+	const sitekey = "90ac8810-80ba-42f6-abd9-c7ab8cef95e1"
+	// const sitekey = "10000000-ffff-ffff-ffff-000000000001"
 
 	function handleVerificationSuccess(token, ekey) {
 		setToken(token)
@@ -181,12 +181,14 @@ const Form = (props) => {
 			</label>
 
 			{/* {props.children} */}
-      <HCaptcha
-        ref={captchaRef}
-        sitekey={sitekey}
-        theme="dark"
-        onVerify={(token, ekey) => handleVerificationSuccess(token, ekey)}
-      />
+			<HCaptcha
+				ref={captchaRef}
+				sitekey={sitekey}
+				theme="dark"
+				onVerify={(token, ekey) =>
+					handleVerificationSuccess(token, ekey)
+				}
+			/>
 			<label>
 				<input type="submit" />
 			</label>
