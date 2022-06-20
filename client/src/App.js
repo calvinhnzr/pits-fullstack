f5e3070683da6f1ed2f1ea7f5d610b5ef23cc0d3
calvinhnzr <52936430+calvinhnzr@users.noreply.github.com>
2022-06-20 12:55:05 +0200

    [FEATURE] bot detection by side call

--- client/src/App.js
+++ client/src/App.js
@@ -3,7 +3,7 @@ import {
 	BrowserRouter as Router,
 	Route,
 	Routes,
-	useLocation,
+	Navigate,
 } from "react-router-dom"
 
 import About from "./pages/About"
@@ -17,19 +17,55 @@ import Behavoir from "./pages/Behavoir"
 import Nav from "./components/Nav"
 import Footer from "./components/Footer"
 import "./App.css"
-import styled from "styled-components"
-import img from "./images/honeypotBG.png"
 import Success from "./pages/Success"
+import Failure from "./pages/Failure"
 
 function App() {
+	const [botDetected, setBotDetected] = useState(false)
+
+	useEffect(() => {
+		if (/HeadlessChrome/.test(window.navigator.userAgent)) {
+			console.log("Chrome headless detected")
+			setBotDetected(true)
+		}
+		if (navigator.webdriver) {
+			console.log("Chrome headless detected")
+			setBotDetected(true)
+		}
+		if (navigator.plugins.length === 0) {
+			console.log("It may be Chrome headless")
+			setBotDetected(true)
+		}
+		if (navigator.languages === "") {
+			console.log("Chrome headless detected")
+			setBotDetected(true)
+		}
+		navigator.permissions
+			.query({ name: "notifications" })
+			.then(function (permissionStatus) {
+				if (
+					Notification.permission === "denied" &&
+					permissionStatus.state === "prompt"
+				) {
+					console.log("This is Chrome headless")
+					setBotDetected(true)
+				} else {
+					console.log("This is not Chrome headless")
+					setBotDetected(false)
+				}
+			})
+	}, [])
+
 	return (
 		<Router>
 			<div className="App">
+				{botDetected && <Navigate replace to="/failure" />}
 				<Nav />
 				<Routes>
 					<Route path="/" element={<Home />} />
 					<Route path="about" element={<About />} />
 					<Route path="success" element={<Success />} />
+					<Route path="failure" element={<Failure />} />
 					<Route path="/tickets">
 						<Route path="honeypot" element={<Honeypot />} />
 						<Route path="captcha" element={<Captcha />} />
